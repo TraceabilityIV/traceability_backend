7ed2136398900480a810ca0bdcaf9334b18ec61e
Fast089609 <fc089609@gmail.com>
2024-02-26 21:11:51 -0500

    dockerizacion

---
+++ database/migrations/2024_02_26_120047_create_mensajes_chat_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mensajes_chat', function (Blueprint $table) {
+            $table->id();
+            $table->text('mensaje');
+            $table->enum("tipo", ['texto', 'audio', 'imagen', 'video', 'archivo']);
+
+            $table->unsignedBigInteger('usuario_envia_id');
+            $table->foreign('usuario_envia_id')->references('id')->nullOnDelete()->on('users');
+
+            $table->unsignedBigInteger('usuario_recibe_id');
+            $table->foreign('usuario_recibe_id')->references('id')->nullOnDelete()->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mensajes_chat');
+    }
+};
