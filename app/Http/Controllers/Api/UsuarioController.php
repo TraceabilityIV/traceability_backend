c73ea814a5b38b3378690cc2c8c2ed64e5fd86f1
fast089609 <fast089609@gmail.com>
2024-04-03 22:45:24 -0500

    feat: :zap: Cambios de validacion de usuarios

--- app/Http/Controllers/Api/UsuarioController.php
+++ app/Http/Controllers/Api/UsuarioController.php
@@ -57,23 +57,26 @@ class UsuarioController extends Controller
         //cargamos las imagenes en tal caso
         if($request->hasFile('avatar')){
             $campos['avatar'] = $request->file('avatar')->hashName();
-
-            $campos['avatar'] = url('storage/' . $request->file('avatar')->storeAs('usuarios/avatars', $campos['avatar']));
+            $request->file('avatar')->storeAs('public/usuarios/avatars', $campos['avatar']);
+            $campos['avatar'] = url('storage/usuarios/avatars/' . $campos['avatar']);
         }
         if($request->hasFile('doc_identificacion')){
             $campos['doc_identificacion'] = $request->file('doc_identificacion')->hashName();
-
-            $campos['doc_identificacion'] = url('storage/' . $request->file('doc_identificacion')->storeAs('usuarios/doc_identificacion', $campos['doc_identificacion']));
+            $request->file('doc_identificacion')->storeAs('public/usuarios/doc_identificacion', $campos['doc_identificacion']);
+            //obtenemos la ruta de una vez
+            $campos['doc_identificacion'] = url('storage/usuarios/doc_identificacion/' . $campos['doc_identificacion']);
         }
         if($request->hasFile('rut')){
             $campos['rut'] = $request->file('rut')->hashName();
-
-            $campos['rut'] = url('storage/' . $request->file('rut')->storeAs('usuarios/ruts', $campos['rut']));
+            $request->file('rut')->storeAs('public/usuarios/ruts', $campos['rut']);
+            $campos['rut'] = url('storage/usuarios/ruts/' . $campos['rut']);
         }
         if($request->hasFile('contrato')){
             $campos['contrato'] = $request->file('contrato')->hashName();
 
-            $campos['contrato'] = url('storage/' . $request->file('contrato')->storeAs('usuarios/contratos', $campos['contrato']));
+            $request->file('contrato')->storeAs('public/usuarios/contratos', $campos['contrato']);
+
+            $campos['contrato'] = url('storage/usuarios/contratos/' . $campos['contrato']);
         }
 
         if(isset($campos['doc_identificacion']) && isset($campos['rut']) && isset($campos['contrato'])){
@@ -156,24 +159,26 @@ class UsuarioController extends Controller
 
             if($request->hasFile('avatar')){
                 $campos['avatar'] = $request->file('avatar')->hashName();
-
-                $campos['avatar'] = url('storage/' . $request->file('avatar')->storeAs('usuarios/avatars', $campos['avatar']));
+                $request->file('avatar')->storeAs('public/usuarios/avatars', $campos['avatar']);
+                $campos['avatar'] = url('storage/usuarios/avatars/' . $campos['avatar']);
             }
             if($request->hasFile('doc_identificacion')){
                 $campos['doc_identificacion'] = $request->file('doc_identificacion')->hashName();
-
+                $request->file('doc_identificacion')->storeAs('public/usuarios/doc_identificacion', $campos['doc_identificacion']);
                 //obtenemos la ruta de una vez
-                $campos['doc_identificacion'] = url('storage/' . $request->file('doc_identificacion')->storeAs('usuarios/doc_identificacion', $campos['doc_identificacion']));
+                $campos['doc_identificacion'] = url('storage/usuarios/doc_identificacion/' . $campos['doc_identificacion']);
             }
             if($request->hasFile('rut')){
                 $campos['rut'] = $request->file('rut')->hashName();
-
-                $campos['rut'] = url('storage/' . $request->file('rut')->storeAs('usuarios/ruts', $campos['rut']));
+                $request->file('rut')->storeAs('public/usuarios/ruts', $campos['rut']);
+                $campos['rut'] = url('storage/usuarios/ruts/' . $campos['rut']);
             }
             if($request->hasFile('contrato')){
                 $campos['contrato'] = $request->file('contrato')->hashName();
 
-                $campos['contrato'] = url('storage/' . $request->file('contrato')->storeAs('usuarios/contratos', $campos['contrato']));
+                $request->file('contrato')->storeAs('public/usuarios/contratos', $campos['contrato']);
+
+                $campos['contrato'] = url('storage/usuarios/contratos/' . $campos['contrato']);
             }
 
             if(isset($campos['doc_identificacion']) && isset($campos['rut']) && isset($campos['contrato'])){
